fb7f0386f5a56854493f24909f2732eee7af3c30
pacifika <pacifika@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-10 09:56:14 +0000

    * updated readme legibility
    git-svn-id: https://plugins.svn.wordpress.org/hide-trackbacks/tags/1.0.1@383146 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hide-trackbacks.php
@@ -0,0 +1,94 @@
+<?php
+/*
+Plugin Name: Hide Trackbacks
+Plugin URI: http://wp.me/p1vXha-4u
+Description: Stops trackbacks and pingbacks from showing up as comments on your posts.
+Version: 1.0
+Author: Sander van Dragt
+Author URI: http://amasan.co.uk/blog
+License: GPL2
+
+	  Copyright 2011  Sander van Dragt  (email : sander.vandragt@gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+	Derived from original code by:
+  Honey Singh 
+  http://www.honeytechblog.com/how-to-remove-tracbacks-and-pings-from-wordpress-posts/
+
+*/
+
+class SVD_HideTrackbacks {
+
+	// Initialisation
+	function SVD_HideTrackbacks() {
+		add_filter( 'the_posts',           array( &$this, 'filter_post_comments' ) );
+		add_filter( 'comments_array',      array( &$this, 'filter_trackbacks' ) );
+		add_filter( 'get_comments_number', array( &$this, 'filterCommentCount' ) );
+	}
+
+
+	// Updates the comment number for posts with trackbacks
+	function filter_post_comments( $posts ) {
+		foreach ( $posts as $key => $p ) {
+			if ( $p->comment_count <= 0 ) { 
+				return $posts; 
+			}
+			$posts[$key]->comment_count = $this->_count_comments( (int) $p->ID );
+		}
+		return $posts;
+	}
+
+
+	// Updates the count for comments and trackbacks
+	function filter_trackbacks( $comms ) {
+		global $comments;
+		$comments = $this->_strip_trackbacks( $comms );
+		return $comments;
+	}
+
+
+	// Strips out trackbacks/pingbacks
+	function strip_trackback( $var ) {
+		if ( $var->comment_type == 'trackback' || $var->comment_type == 'pingback' ) { 
+			return false; 
+		}
+		return true;
+	}
+
+
+	// Return the correct comment count within the loop
+	function filterCommentCount( $commentcount ) {
+		$id = get_the_ID();
+		return $this->_count_comments( $id );
+	}
+
+	// Helper  for  counting comments per post
+	private function _count_comments( $id ) {
+		$comments = get_approved_comments( $id );
+		$comments = $this->_strip_trackbacks( $comments );
+		return sizeof( $comments );
+	}
+
+	// helper for filtering out the trackbacks / pingbacks leaving comments only from list of comments
+	private function _strip_trackbacks( $comms ) {
+		if ( !is_array( $comms ) ) { 
+			return;
+		}
+		return array_filter( $comms,  array( &$this, 'strip_trackback' ) );
+	}
+}
+
+// Load the plugin
+add_action( 'plugins_loaded', create_function( '', 'global $svd_hide_trackbacks; $svd_hide_trackbacks = new SVD_HideTrackbacks();' ) );
---
+++ readme.txt
@@ -0,0 +1,31 @@
+=== Hide Trackbacks ===
+Contributors: pacifika
+Tags: comments, trackbacks, pingbacks, trackback, spam
+Requires at least: 3.1.2
+Tested up to: 3.1.2
+Stable tag: 1.0.1
+
+Stops trackbacks and pingbacks from showing up as comments on posts. 
+
+== Description ==
+
+Introducing *Hide Trackbacks*. You can have the benefits of track/ping backs (know when someone writes about posts) whilst keeping the comments clean and uncluttered.
+
+After enabling the plugin, trackbacks and pingbacks are no longer shown on your posts and the comment count is updated correctly to reflect this. You can still access them via the admin panel. NOTE: Although the plugin officially requires WordPress 3.1.2 it might very well work on older WordPress versions (if it does please let me know).
+
+Original code created by  [Honey Singh](http://www.honeytechblog.com/how-to-remove-tracbacks-and-pings-from-wordpress-posts/) (used with permission of the author).
+
+== Installation ==
+
+1. Upload the `hide-trackbacks` directory to `/wp-content/plugins/`.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+
+
+== Changelog ==
+
+= 1.0.1 =
+* Improved readme legibility
+
+= 1.0 =
+* Initial version
+
